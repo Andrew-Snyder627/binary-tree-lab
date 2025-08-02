226d9796df344e69769ef5f14fd4f8d4c048c773
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-08-02 19:46:07 -0400

    max_depth recursive function in place

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -8,7 +8,13 @@ class TreeNode:
 
 # TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if root is None:
+        return 0
+    # Recursively get depth of left and right subtree
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+
+    return max(left_depth, right_depth) + 1 # Add 1 to include the current node
 
 # TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
